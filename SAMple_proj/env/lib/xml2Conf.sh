f3a320fa09db3ecf66ca5fe1dd0506fd6845992e
UnixBear <unixbear@gmail.com>
2022-06-29 20:35:22 -0700

    removed env folder to avoid bloat

--- SAMple_proj/env/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/soc/Projects/DataScience_Bootcamp/SAMple_proj/env/lib"
-XML2_LIBS="-lxml2 -L/home/soc/Projects/DataScience_Bootcamp/SAMple_proj/env/lib -lz -L/home/soc/Projects/DataScience_Bootcamp/SAMple_proj/env/lib -llzma -lpthread -L/home/soc/Projects/DataScience_Bootcamp/SAMple_proj/env/lib -L/home/soc/Projects/DataScience_Bootcamp/SAMple_proj/env/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/home/soc/Projects/DataScience_Bootcamp/SAMple_proj/env/include/libxml2 -I/home/soc/Projects/DataScience_Bootcamp/SAMple_proj/env/include"
-MODULE_VERSION="xml2-2.9.12"
-
