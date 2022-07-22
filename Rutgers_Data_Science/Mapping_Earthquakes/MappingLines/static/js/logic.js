2a69c9592dbc7a2dbf46de467baae19690381755
UnixBear <unixbear@gmail.com>
2022-07-21 23:35:16 -0700

    Created an polyline outlining a trip from LAX to SFO to SLC and finally to SEA

--- Rutgers_Data_Science/Mapping_Earthquakes/MappingLines/static/js/logic.js
+++ Rutgers_Data_Science/Mapping_Earthquakes/MappingLines/static/js/logic.js
@@ -1,14 +1,17 @@
 console.log("working");
 
-//Setting center of the map approximately between LAX and SFO [36.1733, -120.1794]
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
-    // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let map = L.map('mapid', {
+    center: [
+        40.7, -94.5
+    ],
+    zoom: 4,
+});
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
-
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
@@ -22,9 +25,6 @@ let cityData = cities
 //    fillColor: '#ffffa1'
 //}).addTo(map);
 
-
-// This adds a marker with a popup showing each city's population
-/*
 cityData.forEach(function (city) {
     L.circleMarker(city.location, {
         radius: city.population / 200000,
@@ -35,19 +35,5 @@ cityData.forEach(function (city) {
     L.marker(city.location)
         .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
         .addTo(map);
-})*/
+})
 
-//these are the coordinates of LAX and SFO respectively
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
-
-//adding a marker over LAX and SFO
-line.forEach(airport => L.marker(airport).addTo(map));
-  
-L.polyline(line, {
-    color: "yellow"
-}).addTo(map);
