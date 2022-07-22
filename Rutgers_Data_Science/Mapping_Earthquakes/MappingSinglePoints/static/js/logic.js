2b05be4afb7f7db11dcca5a92146026b2bf8de5b
UnixBear <unixbear@gmail.com>
2022-07-22 02:11:27 -0700

    make logic behind logic.js clearer

--- Rutgers_Data_Science/Mapping_Earthquakes/MappingSinglePoints/static/js/logic.js
+++ Rutgers_Data_Science/Mapping_Earthquakes/MappingSinglePoints/static/js/logic.js
@@ -2,7 +2,7 @@ console.log("working");
 
 //Setting center of the map approximately between LAX and SFO [36.1733, -120.1794]
 let map = L.map('mapid').setView([37.6213, -122.3790], 5);
-    // We create the tile layer that will be the background of our map.
+        // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
